187a94efd29c80ce3ce4df7951ec8b56a8b02d3b
Hassanin Ahmed <sas1ni69@gmail.com>
2019-05-31 15:53:19 +0800

    initial commit

---
+++ api/__init__.py
@@ -0,0 +1,7 @@
+from flask_restful import Api
+from app import flaskAppInstance
+from .ProjectAPI import ProjectAPI
+
+restServerInstance = Api(flaskAppInstance)
+
+restServerInstance.add_resource(ProjectAPI, "/")
